a103da5e698bf5da50bd3ea8574c96644baacdb8
진용경 <91701751+thangyk@users.noreply.github.com>
2022-07-15 10:16:42 +0900

    [Bronze III] Title: 영수증, Time: 216 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int total = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int index = sc.nextInt();
+            sum += index;
+        }
+
+        System.out.println(total - sum);
+
+
+    }
+}
