4447e44d3a008773b47f9449aa793575d51962ff
진용경 <91701751+thangyk@users.noreply.github.com>
2022-07-22 03:24:54 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 592 ms, Memory: 296152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,47 +1,32 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        Queue queue = new LinkedList();
-        ArrayList<Integer> list = new ArrayList();
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<Integer>();
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-        IntStream.range(1, n + 1).forEach(x -> queue.add(x));
-
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-            int data = (int) queue.remove();
-            cnt++;
-            if (cnt % k == 0) {
-                list.add(data);
-            } else {
-                queue.add(data);
-            }
+        for (int i = 0; i < n; i++) {
+            q.add(i + 1);
         }
-
-        bw.write("<");
-        for (int i = 0; i < list.size()-1; i++) {
-            bw.write(list.get(i)+ ", ");
+        sb.append("<");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k - 1; j++)
+                q.add(q.poll());
+            sb.append(q.poll() + ", ");
         }
-        bw.write(list.get(list.size()-1) + ">\n");
-
-        bw.flush();
+        sb.setLength(sb.length() - 2);
+        sb.append(">");
+        System.out.println(sb.toString());
         br.close();
-        bw.close();
-
     }
 }
