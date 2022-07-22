417044cd1424823b1c919a52d11ac90cdfbb6e55
진용경 <91701751+thangyk@users.noreply.github.com>
2022-07-22 15:33:45 +0900

    [Bronze I] Title: 단어 공부, Time: 472 ms, Memory: 30128 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int[] alphabet = new int[26];
+
+        String input = sc.next().toUpperCase();
+        sb.append(input);
+
+        for (int i = 0; i < sb.length(); i++) {
+            int num = sb.charAt(i) - 65;
+            alphabet[num]++;
+        }
+
+        int max = 0;
+        char c = ' ';
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] > max) {
+                max = alphabet[i];
+                c = (char) (i + 65);
+            } else if (alphabet[i] == max) {
+                c = '?';
+            }
+        }
+
+        System.out.println(c);
+
+    }
+}
