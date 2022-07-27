480aa8e713a9611b8cc78179b3f0ceba57abab02
진용경 <91701751+thangyk@users.noreply.github.com>
2022-07-27 17:20:14 +0900

    [Silver III] Title: 빈도 정렬, Time: 188 ms, Memory: 15060 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+
+        ArrayList<Integer> v = new ArrayList<Integer>(map.keySet());
+
+        //배열에 저장된 키들의 값을 value값으로 내림차순 정렬한다.
+        Collections.sort(v, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                //list.get(b) 와 list.get(a)의 위치가 바뀌면 오름차순이 된다.
+                return Integer.compare(map.get(b), map.get(a));
+            }
+        });
+        //Iterator를 통해서 출력한다.
+        Iterator<Integer> it = v.iterator();
+        while (it.hasNext()) {
+            Integer element = it.next();
+            for (int i = 0; i < map.get(element); i++) {
+                System.out.print(element + " ");
+            }
+
+        }
+
+
+        br.close();
+    }
+
+}
