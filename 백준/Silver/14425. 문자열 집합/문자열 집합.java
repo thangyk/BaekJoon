2c6e6e536c854a1e1d1ac99335a620ea86aa39dc
진용경 <91701751+thangyk@users.noreply.github.com>
2022-08-05 15:39:54 +0900

    [Silver III] Title: 문자열 집합, Time: 420 ms, Memory: 31232 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,36 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static HashSet<String> set = new HashSet<>();
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        String[] sArr = new String[N];
-        for (int i = 0; i < sArr.length; i++) {
-            sArr[i] = br.readLine();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
         }
 
-        int cnt = 0;
         for (int i = 0; i < M; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < N; j++) {
-                if (input.equals(sArr[j])) {
-                    cnt++;
-                    break;
-                }
-
+            if (set.contains(br.readLine())) {
+                cnt++;
             }
         }
-        System.out.println(cnt);
 
+        System.out.println(cnt);
         br.close();
 
     }
