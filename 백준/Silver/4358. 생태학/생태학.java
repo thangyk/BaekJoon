305b5e8caceb2afa0741459c9ae887643d12338c
진용경 <91701751+thangyk@users.noreply.github.com>
2022-08-05 16:47:13 +0900

    [Silver II] Title: 생태학, Time: 736 ms, Memory: 85468 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        TreeMap<String, Double> map = new TreeMap<>();
+        Map<String, Integer> map = new HashMap<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
@@ -18,12 +20,15 @@ public class Main {
                 break;
             }
             cnt++;
-            map.put(type, map.getOrDefault(type, Double.valueOf(0)) + 1);
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
 
-        for (Map.Entry<String, Double> item : map.entrySet()) {
-            sb.append(item.getKey() + " ");
-            sb.append(String.format("%.4f", (item.getValue()/cnt)*100)+"\n");
+        String[] tree = map.keySet().toArray(new String[0]);
+        Arrays.sort(tree);
+
+        for (int i = 0; i < tree.length; i++) {
+            sb.append(tree[i] + " ");
+            sb.append(String.format("%.4f", (map.get(tree[i])/cnt)*100)+"\n");
         }
 
         System.out.println(sb);
