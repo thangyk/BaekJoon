703f4238ff0e563e5e328fe80e71bdd8b8e57cdd
진용경 <91701751+thangyk@users.noreply.github.com>
2022-08-05 16:37:39 +0900

    [Silver II] Title: 생태학, Time: 1284 ms, Memory: 135236 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        TreeMap<String, Double> map = new TreeMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        double cnt = 0;
+        while (true) {
+            String type = br.readLine();
+            if (type == null || type.equals("")) {
+                break;
+            }
+            cnt++;
+            map.put(type, map.getOrDefault(type, Double.valueOf(0)) + 1);
+        }
+
+        for (Map.Entry<String, Double> item : map.entrySet()) {
+            System.out.printf("%s %.4f\n", item.getKey(),(item.getValue()/cnt)*100);
+        }
+
+
+    }
+}
