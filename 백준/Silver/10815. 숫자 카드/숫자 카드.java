238037107b0446ffe47f7444380177d71026096f
진용경 <91701751+thangyk@users.noreply.github.com>
2022-08-11 19:47:37 +0900

    [Silver V] Title: 숫자 카드, Time: 732 ms, Memory: 117128 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,37 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        boolean[] cardNum = new boolean[20000000];
 
-        Arrays.sort(arr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cardNum[Integer.parseInt(st.nextToken()) + 10000000] = true;
+        }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+            if (cardNum[Integer.parseInt(st.nextToken()) + 10000000] == true) {
                 sb.append(1 + " ");
             } else {
                 sb.append(0 + " ");
             }
         }
+
         System.out.println(sb);
-        
+
         br.close();
     }
 }
