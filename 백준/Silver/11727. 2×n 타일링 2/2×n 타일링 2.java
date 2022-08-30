7fff496e6009d3b7189a0b20823303ad5da3f96c
진용경 <91701751+thangyk@users.noreply.github.com>
2022-08-30 17:15:32 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -14,7 +14,7 @@ public class Main {
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+            dp[i] = (dp[i - 2] + dp[i-2] + dp[i - 1]) % 10007;
         }
 
         System.out.println(dp[n]);
