0ef788b825e233969e3a618552fa75c7748242c5
진용경 <91701751+thangyk@users.noreply.github.com>
2022-10-05 03:38:16 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int ans = 1;
+        for (int i = n; i >= 1; i--) {
+            ans *= i;
+        }
+
+        System.out.println(ans);
+
+        br.close();
+    }
+}
