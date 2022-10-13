00857c46e7f797a35d0934a4d2b80579da07daa8
진용경 <91701751+thangyk@users.noreply.github.com>
2022-10-13 17:08:03 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (isPrime(num)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+        br.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
