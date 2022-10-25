b103460ddaae5c917992be63133d0f3c73efdc4c
진용경 <91701751+thangyk@users.noreply.github.com>
2022-10-25 15:23:34 +0900

    [level 0] Title: 약수 구하기, Time: 2.48 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,19 +1,6 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                cnt++;
-            }
-        }
-        answer = new int[cnt];
-        int idx = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer[idx++] = i;
-            }
-        }
-        return answer;
+        return IntStream.rangeClosed(1,n).filter(i -> n % i == 0).toArray();
     }
 }
