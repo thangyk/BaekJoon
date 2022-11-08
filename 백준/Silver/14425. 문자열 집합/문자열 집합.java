0ab90f30c66b25fcc7cd078a1e86ab05cd967fbf
진용경 <91701751+thangyk@users.noreply.github.com>
2022-11-08 15:08:47 +0900

    [Silver III] Title: 문자열 집합, Time: 404 ms, Memory: 31136 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -2,28 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-    static HashSet<String> set = new HashSet<>();
-    static int cnt;
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < N; i++) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
             set.add(br.readLine());
         }
 
-        for (int i = 0; i < M; i++) {
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
             if (set.contains(br.readLine())) {
                 cnt++;
             }
@@ -31,6 +27,5 @@ public class Main {
 
         System.out.println(cnt);
         br.close();
-
     }
 }
