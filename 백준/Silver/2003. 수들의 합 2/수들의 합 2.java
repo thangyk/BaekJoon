cd07aa47049e63108a368be19cb88bc0a799696d
진용경 <91701751+thangyk@users.noreply.github.com>
2022-11-18 18:34:20 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14964 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,55 +1,41 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[] Arr = new int[N];
-
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < Arr.length; i++) {
-            Arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
         int p1 = 0;
         int p2 = 0;
+
+        int sum = 0;
         int cnt = 0;
-
-        while (p1 < N && p2 < N) {
-            int sum = 0;
-
-            for (int i = p1; i <= p2; i++) {
-                sum += Arr[i];
-            }
-
-            if (sum == M) {
-                p1++;
-                p2++;
-                cnt++;
-                continue;
-            }
-
-            if (sum < M) {
-                p2++;
+        while (true) {
+            if (sum >= m) {
+                sum -= arr[p2++];
+            } else if (p1 >= arr.length) {
+                break;
             } else {
-                p1++;
+                sum += arr[p1++];
             }
 
+            if (sum == m) {
+                cnt++;
+            }
         }
 
-        bw.write(String.valueOf(cnt));
-
-
-        bw.flush();
-        bw.close();
+        System.out.println(cnt);
         br.close();
     }
 }
