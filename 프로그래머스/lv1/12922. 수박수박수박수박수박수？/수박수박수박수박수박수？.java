53c140a6e5681bfff92ae02758d08ff69c68718d
진용경 <91701751+thangyk@users.noreply.github.com>
2023-01-10 00:17:50 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.73 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,17 +1,13 @@
 class Solution {
     public String solution(int n) {
-        String[] arr = new String[n];
-        for (int i = 0; i < arr.length; i++) {
-            if (i % 2 == 0) {
-                arr[i] = "수";
-            } else {
-                arr[i] = "박";
-            }
-        }
-
         String ans = "";
-        for (String s : arr) {
-            ans += s;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                ans += "수";
+            } else {
+                ans += "박";
+            }
         }
 
         return ans;
