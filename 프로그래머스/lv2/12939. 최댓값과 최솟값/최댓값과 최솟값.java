4188b45854ff32dde9d4e8b524264743d916a179
진용경 <91701751+thangyk@users.noreply.github.com>
2023-01-12 01:58:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.60 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+class Solution {
+    public String solution(String s) {
+        StringTokenizer st = new StringTokenizer(s, " ");
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.sort((o1, o2) -> o1 - o2);
+
+        String result = list.get(0) + " " + list.get(list.size() - 1);
+        return result;
+    }
+}
