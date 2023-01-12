713b844f4bb53e7761daea623d161c68c58bac58
진용경 <91701751+thangyk@users.noreply.github.com>
2023-01-12 22:19:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] == true) {
+                sum += absolutes[i];
+            } else {
+                sum += absolutes[i] * -1;
+            }
+        }
+        return sum;
+    }
+}
