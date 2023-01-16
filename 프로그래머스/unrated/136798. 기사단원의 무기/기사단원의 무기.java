d966bddd497591f3fd2aa0086e3980ad8095d0bb
진용경 <91701751+thangyk@users.noreply.github.com>
2023-01-16 16:55:46 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.70 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (prime(i) > limit) {
+                result += power;
+            } else {
+                result += prime(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static int prime(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= (int) Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    cnt++;
+                } else {
+                    cnt += 2;
+                }
+            }
+        }
+        return cnt;
+    }
+}
