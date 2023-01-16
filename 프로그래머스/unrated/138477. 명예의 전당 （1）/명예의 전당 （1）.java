ad39597abd50a5baad889c5d3613f0ccdba18683
진용경 <91701751+thangyk@users.noreply.github.com>
2023-01-16 16:09:32 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.33 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,25 +1,20 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] topSingers = new int[k];
-        Arrays.fill(topSingers, Integer.MIN_VALUE);
-
         int[] result = new int[score.length];
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
         for (int i = 0; i < score.length; i++) {
-            Arrays.sort(topSingers);
+            pq.add(score[i]);
 
-            if (score[i] >= topSingers[0]) {
-                topSingers[0] = score[i];
+            if (pq.size() > k) {
+                pq.poll();
             }
 
-            Arrays.sort(topSingers);
-            if (i < k) {
-                result[i] = topSingers[k - 1 - i];
-            } else {
-                result[i] = topSingers[0];
-            }
+            result[i] = pq.peek();
         }
 
         return result;
