f4c4f4ac4362dc74eb56e6de0b60e9650cea3cf5
진용경 <91701751+thangyk@users.noreply.github.com>
2023-04-18 00:59:23 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int result = 0;
+
+		result += (hp / 5);
+		hp %= 5;
+		result += (hp / 3);
+		hp %= 3;
+		result += hp;
+
+		return result;
+    }
+}
