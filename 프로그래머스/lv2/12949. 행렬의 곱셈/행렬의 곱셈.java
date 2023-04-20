7092e36d8f1df84fbf9d23631cbc0ffda2f91b11
진용경 <91701751+thangyk@users.noreply.github.com>
2023-04-20 17:44:23 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.35 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] result = new int[arr1.length][arr2[0].length];
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				int a = arr1[i][j];
+				for (int k = 0; k < arr2[j].length; k++) {
+					int b = arr2[j][k];
+					result[i][k] += (a * b);
+				}
+			}
+		}
+
+		return result;
+    }
+}
