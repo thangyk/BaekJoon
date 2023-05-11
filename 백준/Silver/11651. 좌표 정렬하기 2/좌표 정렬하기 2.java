d83b1e5ae4b5fa6ebfbea4cd82d813bb5a09f4ba
진용경 <91701751+thangyk@users.noreply.github.com>
2023-05-11 17:39:47 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 716 ms, Memory: 50744 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<XY> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list.add(new XY(x, y));
+		}
+
+		list.sort((o1, o2) -> {
+			if (o1.y == o2.y) {
+				return o1.x - o2.x;
+			} else {
+				return o1.y - o2.y;
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (XY xy : list) {
+			sb.append(xy.x).append(" ").append(xy.y).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	static class XY {
+		int x;
+		int y;
+
+		XY(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
