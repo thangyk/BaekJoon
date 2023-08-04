ca317af85d13fcc168f9bb0c02b87a49df672f62
진용경 <91701751+thangyk@users.noreply.github.com>
2023-08-04 16:33:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 244 ms, Memory: 19660 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -7,19 +7,17 @@ public class Main {
 	static boolean[] visited;
 	static ArrayList<Integer>[] adjList;
 	static StringBuilder sb;
-	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int v = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
 
-		visited = new boolean[n + 1];
 		adjList = new ArrayList[n + 1];
-
 		for (int i = 1; i <= n; i++) {
 			adjList[i] = new ArrayList<>();
 		}
@@ -28,47 +26,50 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
+
 			adjList[s].add(e);
 			adjList[e].add(s);
 		}
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i < n + 1; i++) {
 			Collections.sort(adjList[i]);
 		}
 
 		sb = new StringBuilder();
-		dfs(v);
-		System.out.println(sb.toString());
-		sb = new StringBuilder();
 		visited = new boolean[n + 1];
-		bfs(v);
-		System.out.println(sb.toString());
+		dfs(start);
+		sb.append("\n");
+		visited = new boolean[n + 1];
+		bfs(start);
+		System.out.println(sb);
 	}
 
-	private static void dfs(int v) {
-		visited[v] = true;
-		sb.append(v).append(" ");
-		for (int i : adjList[v]) {
+	public static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(start);
+		visited[start] = true;
+
+		while (!queue.isEmpty()) {
+			int tmp = queue.poll();
+			sb.append(tmp).append(" ");
+			for (int item : adjList[tmp]) {
+				if (!visited[item]) {
+					queue.add(item);
+					visited[item] = true;
+				}
+			}
+		}
+	}
+
+	public static void dfs(int start) {
+		sb.append(start).append(" ");
+		visited[start] = true;
+
+		for (int i : adjList[start]) {
 			if (!visited[i]) {
 				dfs(i);
 			}
 		}
-	}
 
-	private static void bfs(int v) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(v);
-		visited[v] = true;
-
-		while (!queue.isEmpty()) {
-			int nowV = queue.poll();
-			sb.append(nowV).append(" ");
-			for (int i : adjList[nowV]) {
-				if (!visited[i]) {
-					queue.add(i);
-					visited[i] = true;
-				}
-			}
-		}
 	}
 }
