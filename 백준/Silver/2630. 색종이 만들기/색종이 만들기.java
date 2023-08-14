77f608ff4dc55ade2d590a8e9c1c54c557542b31
진용경 <91701751+thangyk@users.noreply.github.com>
2023-08-14 11:01:14 +0900

    [Silver II] Title: 색종이 만들기, Time: 160 ms, Memory: 15692 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,46 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] colors = new int[2];
-    static int[][] board;
-    static void cut(int x, int y, int size){
-        if(size==1) {
-            colors[board[x][y]]++; return;
+    static int white = 0;
+    static int blue = 0;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        int sum = 0;
-        for(int i=0;i<size;i++)
-            for(int j=0;j<size;j++)
-                sum+=board[i+x][j+y];
-        if(sum==0) colors[0]++;
-        else if(sum==size*size) colors[1]++;
-        else{
-            size/=2;
-            cut(x,y,size);
-            cut(x+size,y,size);
-            cut(x,y+size,size);
-            cut(x+size,y+size,size);
-        }
+
+        div(arr, N, 0, 0);
+        StringBuilder sb = new StringBuilder();
+        sb.append(white).append('\n').append(blue);
+        System.out.println(sb);
+
+        br.close();
     }
-    public static void main(String args[]) throws Exception {
-        int n = readInt();
-        board = new int[n][n];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                board[i][j] = readInt();
-        cut(0,0,n);
-        System.out.println(colors[0]+"\n"+colors[1]);
-    }
-    
-    static int readInt() throws IOException {
-        int n = 0;
-        int input;
-        while((input = System.in.read()) <= 32);
-        while (true) {
-            if (input>47 && input<58)
-                n = (n<<3) + (n<<1) + (input&15);
-            else return n;
-            input = System.in.read();
+
+    public static boolean isCheck(int[][] arr, int N, int row, int col) {
+        for (int i = row; i < row + N; i++) {
+            for (int j = col; j < col + N; j++) {
+                if (arr[row][col] != arr[i][j]) {
+                    return false;
+                }
+            }
         }
+        return true;
+    }
+
+    public static void div(int[][] arr, int N, int row, int col) {
+        if (N == 1 || isCheck(arr, N, row, col)) {
+            if (arr[row][col] == 1) {
+                blue++;
+            } else {
+                white++;
+            }
+            return;
+        }
+
+
+        div(arr, N / 2, row, col);
+        div(arr, N / 2, row, col + N / 2);
+        div(arr, N / 2, row + N / 2, col);
+        div(arr, N / 2, row + N / 2, col + N / 2);
     }
 }
