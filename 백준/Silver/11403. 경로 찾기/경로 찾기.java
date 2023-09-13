6b632f21f5d482ef6bfea672cf0ce98412caf594
thangyk <91701751+thangyk@users.noreply.github.com>
2023-09-13 14:43:08 +0900

    [Silver I] Title: 경로 찾기, Time: 180 ms, Memory: 15724 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -6,16 +6,13 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int n = Integer.parseInt(br.readLine());
 		boolean[][] arr = new boolean[n][n];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				if (Integer.parseInt(st.nextToken()) == 1) {
-					arr[i][j] = true;
-				}
+				arr[i][j] = st.nextToken().equals("1");
 			}
 		}
 
@@ -32,11 +29,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (arr[i][j]) {
-					sb.append(1).append(" ");
-				} else {
-					sb.append(0).append(" ");
-				}
+				sb.append(arr[i][j] ? 1 : 0).append(" ");
 			}
 			sb.append("\n");
 		}
