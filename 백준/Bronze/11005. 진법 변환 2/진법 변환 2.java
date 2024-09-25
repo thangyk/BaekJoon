036b89436e4d4e903404a1fba8efec36b378dd30
thangyk <91701751+thangyk@users.noreply.github.com>
2024-09-25 17:08:42 +0900

    [Bronze I] Title: 진법 변환 2, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+int N = sc.nextInt();
+int B = sc.nextInt();
+
+StringBuilder sb = new StringBuilder();
+
+while(N != 0) {
+if(N % B >= 10) sb.append((char)((N % B) + 'A' - 10));
+else sb.append(N % B);
+N = N / B;
+}
+sc.close();
+System.out.print(sb.reverse().toString());
+}
+}
