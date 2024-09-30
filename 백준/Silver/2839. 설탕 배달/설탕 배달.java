f2fd208ae607e682ef13e976b901c984c4e874d5
thangyk <91701751+thangyk@users.noreply.github.com>
2024-09-30 16:07:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-        if (n < 3 || n == 4 || n == 7) {
-            System.out.println(-1);
-            return;
-        }
+		if (n == 4 || n == 7) {
+			System.out.println(-1);
+			return;
+		}
 
-        int cnt = 0;
-        while (true) {
-            if (n % 5 == 0) {
-                System.out.println(n / 5 + cnt);
-                break;
-            }
-            n -= 3;
-            cnt++;
-        }
+		if (n % 5 == 0) {
+			System.out.println(n / 5);
+			return;
+		}
 
-        br.close();
-    }
+		int ans = 0;
+		while (true) {
+			if (n % 5 == 0) {
+				System.out.println(n / 5 + ans);
+				return;
+			}
+			
+			n -= 3;
+			ans++;
+		}
+	}
 }
