818de7c8d3757caa1967c2c12abdb2601c5c6444
thangyk <91701751+thangyk@users.noreply.github.com>
2024-10-02 03:58:23 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int tc = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < tc; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			sb.append(a * b / div(a, b)).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	private static int div(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
