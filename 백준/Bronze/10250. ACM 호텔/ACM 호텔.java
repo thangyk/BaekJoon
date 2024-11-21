faa24f139cc475b7e9ce49c6576ee17a377ee198
thangyk <91701751+thangyk@users.noreply.github.com>
2024-11-21 17:48:30 +0900

    [Bronze III] Title: ACM 호텔, Time: 112 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -9,35 +9,21 @@ public class Main {
 
 		int t = Integer.parseInt(br.readLine());
 
-		StringBuilder sb = new StringBuilder();
-		for (int k = 0; k < t; k++) {
+		for (int i = 0; i < t; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
 			int h = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			int n = Integer.parseInt(st.nextToken());
 
-			String[][] arr = new String[w + 1][h + 1];
-			int idx = 1;
-			boolean isCheck = false;
-			for (int i = 1; i <= w; i++) {
-				for (int j = 1; j <= h; j++) {
-					arr[i][j] = j + String.format("%02d", i);
+			int f = n % h;
+			int r = n / h;
 
-					if (idx == n) {
-						sb.append(arr[i][j]).append("\n");
-						isCheck = true;
-						break;
-					}
-					idx++;
-				}
-
-				if (isCheck) {
-					break;
-				}
+			if (f == 0) {
+				System.out.println(h * 100 + r);
+			} else {
+				System.out.println(f * 100 + r + 1);
 			}
 		}
-
-		System.out.println(sb);
 	}
 }
