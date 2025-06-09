d55fbbe098cfe343406e609d47124073cd7f45ea
thangyk <91701751+thangyk@users.noreply.github.com>
2025-06-09 20:31:47 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                index = i + 1; // 1-based index
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
