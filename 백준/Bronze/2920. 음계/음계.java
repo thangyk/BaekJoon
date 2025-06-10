81adb3a09a09b47ff2d8bc4d21fa1316214c7c6f
thangyk <91701751+thangyk@users.noreply.github.com>
2025-06-10 17:07:08 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,28 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[8];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String result = "";
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i+1] == arr[i]+1) {
-                result = "ascending";
-            } else if (arr[i+1] == arr[i]-1) {
-                result = "descending";
-            } else {
-                result = "mixed";
+        for (int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        String ans = "descending";
+        for (int i = 0; i < 7; i++) {
+            if (Math.abs(arr[i] - arr[i+1]) != 1) {
+                ans = "mixed";
                 break;
             }
+
+            if (arr[i] + 1 == arr[i + 1]) {
+                ans = "ascending";
+            }
         }
-        System.out.println(result);
+
+        System.out.println(ans);
     }
 }
